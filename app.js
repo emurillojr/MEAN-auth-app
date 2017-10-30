7a34a67106ea1dd04b20e49711641084d5a0975d
Ernesto Murillo <emurillo@email.neit.edu>
2017-10-30 11:15:23 -0400

    complied to public folder - able to run on local host 3000 node

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ mongoose.connect(config.database);
 
 // On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Connected to database ' + config.database);
+  console.log('Connected to database ' + config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-    console.log('Database error: ' + err);
+  console.log('Database error: ' + err);
 });
 
 // Initialize express
@@ -48,10 +48,14 @@ app.use('/users', users);
 
 // Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid Endpoint');
+  res.send('Invalid Endpoint');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start Server
 app.listen(port, () => {
-    console.log('Server started on port ' + port);
+  console.log('Server started on port ' + port);
 });

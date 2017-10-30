9592510eec1fbc3c85f2d8cfb266c0234b4032a2
Ernesto Murillo <emurillo@email.neit.edu>
2017-10-29 20:46:24 -0400

    Beautify JS code

--- routes/users.js
+++ routes/users.js
@@ -11,50 +11,60 @@ router.post('/register', (req, res, next) => {
     //res.send('REGISTER');
     // new user object
     let newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username,
-      password: req.body.password
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
     });
-    
+
     User.addUser(newUser, (err, user) => {
-        if(err){
-           res.json({success: false, msg:'Failed to register user'});
-           }
-        else{
-           res.json({success: true, msg:'User Registered'}); 
+        if (err) {
+            res.json({
+                success: false,
+                msg: 'Failed to register user'
+            });
+        } else {
+            res.json({
+                success: true,
+                msg: 'User Registered'
+            });
         }
-        
+
     });
 });
-           
+
 // Authentication
 router.post('/authenticate', (req, res, next) => {
     //res.send('AUTHENTICATE');
     // get username and passoord being submitted
     const username = req.body.username;
     const password = req.body.password;
-    
+
     // get user by the username
-    User.getUserByUsername(username, (err, user) =>{
-        if(err) throw err;
+    User.getUserByUsername(username, (err, user) => {
+        if (err) throw err;
         // if no user return send response to client
-        if(!user){
-           return res.json({success: false, msg: 'User not found'});
-           }
+        if (!user) {
+            return res.json({
+                success: false,
+                msg: 'User not found'
+            });
+        }
         // if there is a user check password (password entered, user.hashedpassword)
-        User.comparePassword(password, user.password, (err, isMatch) =>{
-            if(err) throw err;
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
             // if no error 
-            if(isMatch){
+            if (isMatch) {
                 // create token
-                const token = jwt.sign({data: user}, config.secret, {
+                const token = jwt.sign({
+                    data: user
+                }, config.secret, {
                     expiresIn: 604800 // 1 week worth of seconds
                 });
                 // response to front end
                 res.json({
                     sucess: true,
-                    token: 'JWT '+token,
+                    token: 'JWT ' + token,
                     user: {
                         id: user._id,
                         name: user.name,
@@ -62,20 +72,27 @@ router.post('/authenticate', (req, res, next) => {
                         email: user.email
                     }
                 });
-            // if password doesnt match
+                // if password doesnt match
             } else {
-                return res.json({success: false, msg:'Wrong password'});
+                return res.json({
+                    success: false,
+                    msg: 'Wrong password'
+                });
             }
         });
-        
+
     });
-    
+
 });
-               
+
 // Profile
-router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+router.get('/profile', passport.authenticate('jwt', {
+    session: false
+}), (req, res, next) => {
     //res.send('PROFILE');
-    res.json({user: req.user});
+    res.json({
+        user: req.user
+    });
 });
 
 module.exports = router;

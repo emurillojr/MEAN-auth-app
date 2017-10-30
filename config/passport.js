f06a162c7e6d87981a57f18bb9fbd96eaaa62ab7
Ernesto Murillo <emurillo@email.neit.edu>
2017-10-29 21:41:42 -0400

    added routes

--- config/passport.js
+++ config/passport.js
@@ -3,24 +3,24 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 const config = require('../config/database');
 
-module.exports = function(passport){
+module.exports = function (passport) {
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('JWT');
     opts.secretOrKey = config.secret;
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    console.log(jwt_payload);
-        
+        console.log(jwt_payload);
+
         User.getUserById(jwt_payload.data._id, (err, user) => {
-        if (err) {
-            return done(err, false);
-        }  
-        
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            
-        }   
-      });
+            if (err) {
+                return done(err, false);
+            }
+
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+
+            }
+        });
     }));
 }
